d8a06c7344a2f600595ec3a5711d959e101597b0
roshaanmustafa1 <roshaanmustafa1@gmail.com>
2024-08-08 12:31:29 +0400

    Minor update

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -9,7 +9,7 @@ const Homepage = () => {
           <p>
             YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
             SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
-            SHOES.
+            SHOES. Testing
           </p>
           <div className="btn-box">
             <button className="btn btn-shop">Shop Now</button>
